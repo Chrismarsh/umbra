5d1da62eca153097178da49d314b4dfdc215d35a
Chris Marsh <chrismarsh.c2@gmail.com>
2011-03-03 21:07:37 -0600

    Added BSD header to all files
    Removed unneeded vertex.h
    
    Triangulation code now works.
    The main code now properly loads data, sends it to matlab, triangulates it, brings it back, sends it to matlab and plots it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
+// % Copyright (c) 2011, Chris Marsh
+// 	% All rights reserved.
+// 	% 
+// 	% Redistribution and use in source and binary forms, with or without 
+// 	% modification, are permitted provided that the following conditions are 
+// 	% met:
+// % 
+// 	%     * Redistributions of source code must retain the above copyright 
+// 	%       notice, this list of conditions and the following disclaimer.
+// 	%     * Redistributions in binary form must reproduce the above copyright 
+// 	%       notice, this list of conditions and the following disclaimer in 
+// 	%       the documentation and/or other materials provided with the distribution
+// 	%     * Neither the name of the University of Saskatchewan nor the names 
+// 	%       of its contributors may be used to endorse or promote products derived 
+// 	%       from this software without specific prior written permission.
+// 	%       
+// 	% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// 	% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// 	% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// 	% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// 	% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// 	% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// 	% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// 	% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// 	% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// 	% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// 	% POSSIBILITY OF SUCH DAMAGE.
+
+
 #include <iostream>
 #include <string.h>
 #include <vector>
@@ -65,7 +94,7 @@ int main()
 			std::vector<int> v = tri->get_tri(i);
 			domain_ptr[i*3+0] = v[0];
 			domain_ptr[i*3+1] = v[1];
-			domain_ptr[i*3+1] = v[2];
+			domain_ptr[i*3+2] = v[2];
 		}
 
 		engine->put("new_tri",domain);
@@ -73,7 +102,7 @@ int main()
 
 
 
-		int handle = gfx->plot_patch("square_nodes_5m","new_tri","square_nodes_5m(:,3)");
+		int handle = gfx->plot_patch("[square_nodes_5m(:,1) square_nodes_5m(:,2) square_nodes_5m(:,3)]","new_tri","square_nodes_5m(:,3)");
 		std::cout << "Plotted with handle " << handle << std::endl;
 
 		std::string lol;
